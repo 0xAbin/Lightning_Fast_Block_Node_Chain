8c3bbd4fa8ffa97207842fd96d9eb9d0696abfd7
Abin <abinbvinod@gmail.com>
2023-03-31 16:15:25 +0530

    Next

---
+++ Website/TestWeb/website/app/page.tsx
@@ -0,0 +1,11 @@
+
+
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className = "text-2xl">"Hello World" </h1>
+    </main>
+
+  )
+}
---
+++ Website/TestWeb/website/components/Navbar.tsx
@@ -0,0 +1,22 @@
+"use client"
+import React, { useState } from "react"
+import { Link } from 'react-scroll/modules'
+import { useTheme } from "next-themes"
+import { RiMoonFill, RiSunLine}  from 'react-icons/ri'
+import { IoMdMenu, IoMdClose } from "react-icons/io"
+
+
+interface NavItem {
+    label: string
+    page:string
+}
+
+
+
+
+const Navbar = () => {
+    return  (
+        <div>Navbar</div>
+    )
+}
+export default Navbar
